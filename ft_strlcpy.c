7204c77ff27ef7a3074107783f284595edc3c805
Enrique Murillo Orosco <emurillo@c1r14s6.42lausanne.ch>
2024-10-02 12:36:14 +0200

    lcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,30 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:39:18 by emurillo          #+#    #+#             */
-/*   Updated: 2024/10/01 17:50:18 by emurillo         ###   ########.fr       */
+/*   Updated: 2024/10/02 12:35:47 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
-char	ft_strcpy(char *dest, const char *src)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	int	i;
-	int	j;
+	unsigned int	i;
+	size_t			len;
 
 	i = 0;
-	j = 0;
-	while (src[i] != '\0')
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	while ((i <= size - 1) && src[i] != '\0')
 	{
-		dest[j] = src[i];
+		dest[i] = src[i];
 		i++;
-		j++;
 	}
-	dest[j] = '\0';
-	return (dest);
+	dest[i] = '\0';
+	return (len);
 }
+
+/* int	test(size_t size)
+{
+	char	string[] = "El gato bebe leche";
+	char	buffer[20];
+	size_t		r;
+
+	r = ft_strlcpy(buffer, string, size);
+	printf("copied '%s' into '%s', length %zu\n", string, buffer, r);
+	return (0);
+}
+
+int	main(void)
+{
+	test(18);
+	test(19);
+	test(5);
+	test(8);
+	return (0);
+}
+*/
