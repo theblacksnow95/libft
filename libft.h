7c3213fcca7f78b34865694f3a547f0da93a419a
theblacksnow95 <enrique.murillo24@gmail.com>
2024-10-03 18:33:12 +0200

    prototype

--- libft.h
+++ libft.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yourlogin <youremail@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/03 17:07:04 by yourlogin         #+#    #+#             */
-/*   Updated: 2024/10/03 17:07:04 by yourlogin        ###   ########.ch       */
+/*   Created: 2024/10/03 18:32:23 by yourlogin         #+#    #+#             */
+/*   Updated: 2024/10/03 18:32:32 by yourlogin        ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ size_t	ft_strlcat(char *dst, char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(char *str);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
+char	*ft_strchr(char *str, int search_str);
 
 #endif
