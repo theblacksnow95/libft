799620f899d1219d6e1242e569ebd66384ffb93f
theblacksnow95 <enrique.murillo24@gmail.com>
2024-10-10 17:34:18 +0200

    ce

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 12:59:24 by yourlogin         #+#    #+#             */
-/*   Updated: 2024/10/10 13:44:28 by emurillo         ###   ########.fr       */
+/*   Updated: 2024/10/10 17:33:51 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,18 @@ char	*ft_strchr(char *str, int search_chr);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strnstr(char *big, char *little, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *string);
+char	**ft_split(char const *s, char c);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *buf, int c, size_t len);
 void	*ft_calloc(size_t nitems, size_t size);
-char	*ft_strdup(const char *string);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dst, char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(char *str);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 
 #endif
