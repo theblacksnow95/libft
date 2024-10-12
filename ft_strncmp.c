78e42c9d07c61cdf7889ee6441007bafe0a1eee1
theblacksnow95 <enrique.murillo24@gmail.com>
2024-10-12 15:40:16 +0200

    goodcmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:43:34 by yourlogin         #+#    #+#             */
-/*   Updated: 2024/10/12 15:28:12 by emurillo         ###   ########.fr       */
+/*   Updated: 2024/10/12 15:40:07 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,18 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && str1[i] != '\0' && str2[i] != '\0')
+	while (i < n && (str1[i] != '\0' || str2[i] != '\0'))
 	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+		if ((unsigned char)str1[i] != (unsigned char)str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 		i++;
 	}
-	if (i != n)
-		return (str1[i] - str2[i]);
 	return (0);
 }
 
-int	main(void)
+/* int	main(void)
 {
-	printf("%i", ft_strncmp("test\200", "test\00", 6));
+	printf("%i", ft_strncmp("abcdef", "abc\375xx", 6));
 	return (0);
 
-}
+} */
