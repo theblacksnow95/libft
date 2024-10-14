3ac17bc7dca922b057b57328213dc363cc563aa1
theblacksnow95 <enrique.murillo24@gmail.com>
2024-10-14 18:23:32 +0200

    join

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 12:59:24 by yourlogin         #+#    #+#             */
-/*   Updated: 2024/10/14 18:11:57 by emurillo         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:23:25 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int		ft_memcmp(const void *buf1, const void *buf2, size_t c);
 char	*ft_itoa(int n);
 char	*ft_strchr(char *str, int search_chr);
 char	*ft_strrchr(const char *str, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strnstr(char *big, char *little, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
