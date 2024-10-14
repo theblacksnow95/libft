7874785d3868089fb8bf511437a7818a142e537b
theblacksnow95 <enrique.murillo24@gmail.com>
2024-10-14 13:04:26 +0200

    ok rchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yourintraname <yourintraname@student.42Ber +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/10 11:53:18 by yourlogin         #+#    #+#             */
-/*   Updated: 2024/10/04 14:11:37 by yourintraname    ###   ########.fr       */
+/*   Created: 2024/10/14 12:31:47 by yourlogin         #+#    #+#             */
+/*   Updated: 2024/10/14 13:03:56 by yourintraname    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,34 @@ char	*ft_strrchr(const char *str, int c)
 	int	i;
 
 	i = 0;
+	c = c & 0xFF;
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	i--;
-	while (str[i] != '\0')
+	while (i >= 0)
 	{
 		if (str[i] == c)
-		{
 			return ((char *)&str[i]);
-		}
 		i--;
 	}
-	i = 0;
 	if (c == '\0')
 	{
-		return ((char *)&str[i]);
+		return ((char *)str);
 	}
 	return (NULL);
 }
 
-/*
-int	main(void)
+
+/*  int	main(void)
 {
 	char	str[] = "welcome to the university";
 	char	c;
 	char	*res;
 
-	c = 'y';
+	c = 't' + 256;
 	res = ft_strrchr(str, c);
 	printf("string after '%c' is '%s'\n", c, res);
 	return (0);
-} */
+}
+ */
